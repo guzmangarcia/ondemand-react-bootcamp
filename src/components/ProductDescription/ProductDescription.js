1de0bce47a14a1e30f2ca5e688b8bd5433efb204
guzmangarcia <guzman.garcia@wizeline.com>
2022-07-25 12:54:36 +0200

    adds style to product ProductDescription

--- src/components/ProductDescription/ProductDescription.js
+++ src/components/ProductDescription/ProductDescription.js
@@ -5,16 +5,16 @@ export default function ProductDescription({ name, price, sku, categoryName, tag
 
     return (
         <div>
-            <p><label>Name: {name}</label></p>
-            <p><label>Price: {price}</label></p>
-            <p><label>SKU: {sku}</label></p>
-            <p><label>Category: {categoryName}</label></p>
-            <p><label>Tags: {tags.map((tag, id) => { return (<label key={id}>{tag} </label>) })}</label></p>
-            <p><label>Description: <br /> {description}</label></p>
-            <div>Qty: <input type={'number'}  />  <button >Add to cart</button></div>
-            <div><p>Specs:</p>
+            <p><label><b>Name: </b>{name}</label></p>
+            <p><label><b>Price:</b> {price}</label></p>
+            <p><label><b>SKU:</b> {sku}</label></p>
+            <p><label><b>Category:</b> {categoryName}</label></p>
+            <p><label><b>Tags:</b> {tags.map((tag, id) => { return (<label key={id}>{tag} </label>) })}</label></p>
+            <p><label><b>Description:</b> <br /><br /> {description}</label></p>
+            <div><b>Qty:</b> <input type={'number'}  />  <button >Add to cart</button></div>
+            <div><p><b>Specs:</b></p>
                 <ul>
-                    {specs.map((spec, index) => { return (<li key={index}>{spec.spec_name}: {spec.spec_value}</li>) })}
+                    {specs.map((spec, index) => { return (<li key={index}><b>{spec.spec_name}</b>: {spec.spec_value}</li>) })}
                 </ul>
             </div>
         </div>
