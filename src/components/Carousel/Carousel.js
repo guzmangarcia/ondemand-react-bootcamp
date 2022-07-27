250e5dfa6db5317c427668c1c26fef4498e4f69f
guzmangarcia <guzman.garcia@wizeline.com>
2022-07-27 16:07:04 +0200

    moved to other file

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,51 +1,7 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 
 import styles from './Carrousel.module.scss'
-import { useNavigate } from 'react-router-dom'
-
-function CarouselElement({ className, count, carouselCurrentSlideIndex, src, alt, text, id, navigationLink }) {
-
-
-    let itemRef = React.createRef()
-    const navigate = useNavigate();
-
-    useEffect(() => {
-
-        itemRef.current.style.display = (id === carouselCurrentSlideIndex) ? 'block' : 'none';
-    }, [id, carouselCurrentSlideIndex, itemRef])
-
-
-    return (
-        <div ref={itemRef} className={styles.carrouselMainStyle} >
-            <div className={`${styles.fade}`}>
-
-                <div>
-                    <img src={src}
-                        alt={alt}
-                        onClick={() => {
-
-                            if (navigationLink !== undefined) {
-                                navigate(navigationLink);
-
-                            }
-
-                        }}
-                        className={`${styles.styleCarrouselImage} ${className}`} />
-                </div>
-                <div className={styles.divtext}>
-                    {text.map((element, index) => {
-                        return <div key={`${text} ${index}`} className={styles.text} >{element} </div>
-                    })}
-
-                    <div className={styles.text}>  ({id}/{count})</div>
-                </div>
-            </div>
-        </div>);
-
-
-
-}
-
+import { CarouselElement } from "./CarouselElement";
 
 export default function Carousel({
     className,
---
+++ src/components/Carousel/CarouselElement.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import styles from './Carrousel.module.scss';
+import { useNavigate } from 'react-router-dom';
+
+export function CarouselElement({ className, count, carouselCurrentSlideIndex, src, alt, text, id, navigationLink }) {
+
+
+    let itemRef = React.createRef();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+
+        itemRef.current.style.display = (id === carouselCurrentSlideIndex) ? 'block' : 'none';
+    }, [id, carouselCurrentSlideIndex, itemRef]);
+
+
+    return (
+        <div ref={itemRef} className={styles.carrouselMainStyle}>
+            <div className={`${styles.fade}`}>
+
+                <div>
+                    <img src={src}
+                        alt={alt}
+                        onClick={() => {
+
+                            if (navigationLink !== undefined) {
+                                navigate(navigationLink);
+
+                            }
+
+                        }}
+                        className={`${styles.styleCarrouselImage} ${className}`} />
+                </div>
+                <div className={styles.divtext}>
+                    {text.map((element, index) => {
+                        return <div key={`${text} ${index}`} className={styles.text}>{element} </div>;
+                    })}
+
+                    <div className={styles.text}>  ({id}/{count})</div>
+                </div>
+            </div>
+        </div>);
+
+
+
+}
